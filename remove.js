0ee1e7b90c46b883e03bd159b8d6a432986282aa
rpruazol <rpruazol@gmail.com>
2020-12-19 01:48:12 -0800

    refactor

--- remove.js
+++ remove.js
@@ -3,32 +3,30 @@
 const fs = require('fs');
 const readline = require('readline');
 const superagent = require('superagent');
-// function readFile(path: string | number | Buffer | URL, callback: (err: NodeJS.ErrnoException, data: Buffer) => void): void (+3 overloads)
-// namespace readFile
+require('dotenv').config();
 
 
-fs.readFile('test.txt', 'utf-8', (error, data) => {
-  if(error) {
-    console.log(error);
-  }
-  else {
-    let newData = [data];
-    newData.forEach(line => {
-      const URL = `https://api.github.com/repos/${line}`;
-      console.log(URL);
-      superagent
-        .del(URL)
-        .set('Authorization', `Bearer ${process.env.TOKEN}`)
-        .set('User-Agent', process.env.GITHUB_USER)
-        .then( res => {
-          console.log(res);
-        })
-        .catch( error => {
-          console.log('error', error.response.error)
-        })
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('test.txt'),
+    output: false,
+    console: false
+});
 
-    });
-  }
+readInterface.on('line', line => {
+  const URL = `https://api.github.com/repos/${line}`;
+  console.log('NEW LOOP IN THE FOREACH');
+  superagent
+    .del(URL)
+    .set('Authorization', `Bearer ${process.env.TOKEN}`)
+    .set('User-Agent', process.env.GITHUB_USER)
+    .then( res => {
+      console.log('res', res);
+    })
+    .catch( error => {
+      console.log('error', error.response.error)
+    })
 })
+
+
 
 
